593f75e7fdca0f8702399022450519fdaa4095ea
Padmavathi9 <padmavathit@virtusa.com>
2019-05-20 12:48:11 +0530

    Completed the module

--- src/Quiz.js
+++ src/Quiz.js
@@ -9,8 +9,8 @@ class Quiz extends Component {
     }
     render(){
         return(
-            <div><div className="quizQuestion">{quizData.quiz_questions[0].instruction_text}
-            </div></div>
+            <div className="quizQuestion">{quizData.quiz_questions[0].instruction_text}
+            </div>
         );
     }
 }
